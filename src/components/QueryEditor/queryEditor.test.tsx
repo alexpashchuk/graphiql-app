f25015d4f623b263f6c832916a9eaec39de67498
olgamartinchik <sintcerova@mail.ru>
2024-01-05 13:41:09 +0300

    fix: update tests

--- src/components/QueryEditor/queryEditor.test.tsx
+++ src/components/QueryEditor/queryEditor.test.tsx
@@ -3,7 +3,11 @@ import '@testing-library/jest-dom';
 import QueryEditor from './queryEditor';
 import { renderWithProviders } from '@/test/test-utils';
 
-import { queryEditorExtension } from '@/mocks/MockSchema';
+import { GraphQLSchema } from 'graphql/type';
+import { Extension } from '@uiw/react-codemirror';
+import { graphql } from 'cm6-graphql';
+import { mockGraphQLSchema, mockSchemaQuery } from '@/mocks/MockSchema';
+import { vi } from 'vitest';
 
 const value = `query getAllCharacters($page: Int, $filter: FilterCharacter) {
   characters(page: $page, filter: $filter) {
@@ -16,7 +20,15 @@ const value = `query getAllCharacters($page: Int, $filter: FilterCharacter) {
     }
   }
 }`;
+const queryEditorExtension = vi.fn((schema?: GraphQLSchema): Extension[] => {
+  const extensions: Extension[] = [];
 
+  if (schema) {
+    extensions.push(graphql(schema));
+  }
+
+  return extensions;
+});
 describe('QueryEditor component', () => {
   it('renders with the provided value and placeholder', async () => {
     const placeholder = 'Enter your query...';
